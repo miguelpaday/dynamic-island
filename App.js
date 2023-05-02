1f2ec5f962d4124f993e15fa7b174ff9742aaa32
Miguel Paday <miguelpaday@pop-os.localdomain>
2023-05-03 01:50:56 +0400

    initial commit

--- App.js
+++ App.js
@@ -5,6 +5,8 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+    
+
       <StatusBar style="auto" />
     </View>
   );
@@ -13,7 +15,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'green',
     alignItems: 'center',
     justifyContent: 'center',
   },
