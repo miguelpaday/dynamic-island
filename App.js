d28f937a0e18a611b487475cef732cf396233eab
Miguel Paday <miguelpaday12@gmail.com>
2023-05-04 02:58:01 +0400

    add reanimated, animate view size withSpring

--- App.js
+++ App.js
@@ -1,21 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { MainScreen } from './src/screens/mainScreen';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    
+      <MainScreen></MainScreen>
+      {/* <Text>Open up App.js to start working on your app!</Text> */}
 
       <StatusBar style="auto" />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'green',
+    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
